3696cbe386b9ec4d2320dde5d80b56805327cf4f
Adam Pelsoczi <pelsoczad@gmail.com>
2024-02-13 22:07:05 +0100

    ConfigModule and Swagger document builder

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv'
+dotenv.config({ path: './server.env' })
+
+function isProduction(): boolean {
+  return process.env.MODE === 'PROD'
+}
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix("/api")
+  app.useGlobalPipes(new ValidationPipe())
+
+  if (!isProduction()) {
+    const config = new DocumentBuilder()
+      .setTitle('Shopping REST')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('/api', app, document)
+  }
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
