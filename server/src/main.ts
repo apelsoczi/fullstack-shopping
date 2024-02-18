553fdc74231f474bc24abf9a9ed268f1efce6922
Adam Pelsoczi <pelsoczad@gmail.com>
2024-02-18 14:40:39 +0100

    server enable cors to post login

--- server/src/main.ts
+++ server/src/main.ts
@@ -12,6 +12,7 @@ function isProduction(): boolean {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   app.setGlobalPrefix("/api")
   app.useGlobalPipes(new ValidationPipe())
